a6e1967218ce49d16ce9dcb6c9a1ccdd24936095
ChoZinLMM <160577451+ChoZinLMM@users.noreply.github.com>
2024-04-05 10:12:19 +0000

    Final Submission

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
-def reverse(text):
+def reverse(text: str) -> str:
     """
+    Takes in a string (text) and reverses the text.
+
+    Parameter
+    ---------
+    text: str
+
+    Returns
+    -------
+    str
+        text reversed
     """
+    #base case
     if len(text) == 0:
       return ''
+        
     else:
        return reverse(text[1:]) + text[0]
-      
-def is_palindrome(str):
+
+
+def is_palindrome(str: str) -> bool:
     """
+    Takes in an input string and checks whether it is a valid palindrome.
+
+    Parameter
+    ---------
+    str: str
+
+    Returns
+    -------
+    boolean
+        True - if str is a valid palindrome
+        False - if str is an invalid palindrome
     """
-    str = str.strip()
-    if len(str) == 0:
-        return ''
+    #strip white spaces and punctuation
+    str = str.strip().strip('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
+    #base case
+    if (len(str) == 0) or (len(str) == 1):
+        return True
+        
     else:
-        
-        
-        
-    
-    
+      if str[0] == str[-1]:
+        str = str.strip(str[0]).strip(str[-1])
+        return is_palindrome(str)
+          
+      else:
+        return False
