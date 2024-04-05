a1344dd87a53ee40bf482081516d0377f55b948d
ChoZinLMM <160577451+ChoZinLMM@users.noreply.github.com>
2024-04-05 08:04:22 +0000

    commit

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-# Built-in imports
-
+def reverse(text):
+    """
+    """
+    if len(text) == 0:
+      return ''
+    else:
+       return reverse(text[1:]) + text[0]
+      
+def is_palindrome(str):
+    """
+    """
+    str = str.strip()
+    if len(str) == 0:
+        return ''
+    else:
+        
+        
+        
+    
+    
